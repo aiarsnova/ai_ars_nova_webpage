632e7142c0a647a7c71e605bd20820ea312a50ff
aiarsnova <aiarsnova@gmail.com>
2024-07-11 03:46:11 +0900

    modify set.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '04f2f14b305743c2a60d570723aeaa3f',
+  rootNotionPageId: 'AI-Ars-Nova-7f43f291585c47358bf23b668295e450',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
